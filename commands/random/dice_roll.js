b49f45e2dca25ff4aa00de8b6f39627e91731f74
Zeref26 <killianpoulain26@gmail.com>
2018-10-22 00:25:28 +0200

    Add files via upload

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -1,5 +1,16 @@
-class DiceRollCommand {
-    async run(message, args){
+const Commando = require('discord.js-commando');
+
+class DiceRollCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name : 'dice',
+            group : 'random',
+            memberName : 'dice',
+            description : 'Lance un dé et renvoie le nombre.'
+        });
+    }
+
+    async run(message, args) {
         var roll = Math.floor(Math.random() * 6) + 1;
         message.reply("Vous êtes tombé sur un" + roll);
     }
--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js-commando');
 const bot = new Discord.Client();
 const config = require('./config.json');
+const prefix = config.prefix;
 
-const mod = require('./commands/moderation/kick.js');
-const de = require('./commands/random/dice_roll.js');
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
 
 bot.on('ready', () => {
     console.log('Bot is ready !');
-  });
+});
 bot.on('message', message => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  switch (command) {
-    case "dice" :
-      var roll = Math.floor(Math.random() * 6) + 1;
-      message.reply("Vous êtes tombé sur un" + roll);
-      break;
-    case "kick" :
-      let reason = args.slice(1).join(" ");
-      const member = message.mentions.members.first();
-      member.kick(reason.join(" "));
-      break;
-    case "say" :
-      let text = args.join(" ");
-      message.channel.send(text);
-      break;
+  if (message.content == prefix+"ping") {
+    message.channel.send("Pong !");
   }
 });
 
