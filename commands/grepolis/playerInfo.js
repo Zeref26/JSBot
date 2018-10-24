ca793977d25a79ef7258398ff2eb22e4950f878a
Zeref26 <killianpoulain26@gmail.com>
2018-10-24 23:47:05 +0200

    Add files via upload

--- commands/grepolis/playerInfo.js
+++ commands/grepolis/playerInfo.js
@@ -27,9 +27,68 @@ class PlayerInfoCommand extends Commando.Command {
                 }
             }
             var i;
-            for(i = 0; i<donnees.length&&arg[1]!=donnees[i]; i++) {
+            for(i = 0; i<donnees.length&&arg.slice(1).join(" ")!=donnees[i]; i++) {
             }
-            message.channel.send("Points de "+arg[1]+" : "+donnees[i+2]);
+            let id;
+            let pseudo;
+            let id_alliance;
+            let points;
+            let rang;
+            let nb_villes;
+            if (i<donnees.length) {
+                id = donnees[i-1];
+                pseudo = donnees[i];
+                id_alliance = donnees[i+1];
+                points = donnees[i+2];
+                rang = donnees[i+3];
+                nb_villes = donnees[i+4];
+            } else {
+                id = "Non défini";
+                pseudo = "Non défini";
+                id_alliance = "Non défini";
+                points = "Non défini";
+                rang = "Non défini";
+                nb_villes = "Non défini";
+
+            }
+            message.channel.send({embed:{
+                color: 0xffffff,
+                author: {
+                  name: "Informations de "+donnees[i],
+                  icon_url: message.channel.client.user.avatarURL
+                },
+                fields: [{
+                    name: "ID",
+                    value: id
+                },
+                {
+                    name: "Pseudo",
+                    value: pseudo
+                },
+                {
+                    name: "ID de l'alliance",
+                    value: id_alliance
+                },
+                {
+                    name: "Points",
+                    value: points
+                },
+                {
+                    name: "Rang",
+                    value: rang
+                },
+                {
+                    name: "Nombre de villes",
+                    value: nb_villes
+                }
+                ],
+                timestamp: new Date(),
+                footer: {
+                  icon_url: message.channel.client.user.avatarURL,
+                  text: "© JS Bot"
+                }
+            }},"",{file: "https://wiki.en.grepolis.com/images/2/20/HoF_Arms.png"}
+            );
         });
     }
 }
