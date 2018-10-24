a5fbbc8359f74b79f2659eb75cc958aa48febbe9
Zeref26 <killianpoulain26@gmail.com>
2018-10-24 21:45:32 +0200

    Add files via upload

--- commands/grepolis/playerInfo.js
+++ commands/grepolis/playerInfo.js
@@ -12,8 +12,25 @@ class PlayerInfoCommand extends Commando.Command {
     }
 
     async run(message, args) {
-        var request = require('request');
-        request('http://fr111.grepolis.com/data/players.txt').pipe(fs.createWriteStream('players.txt'));
+        var fs = require('fs');
+        const arg = message.content.slice(1).trim().split(/ +/g);
+        fs.readFile('players.txt', 'utf8', (err, data) => {
+            if (err) throw err;
+            var mot = "";
+            var donnees = [];
+            for(var i = 0; i<data.length; i++) {
+                if (data.charAt(i)!=','&&data.charAt(i)!='\n') {
+                    mot = mot.concat(data.charAt(i));
+                } else {
+                    donnees.push(mot);
+                    mot = "";
+                }
+            }
+            var i;
+            for(i = 0; i<donnees.length&&arg[1]!=donnees[i]; i++) {
+            }
+            message.channel.send("Points de "+arg[1]+" : "+donnees[i+2]);
+        });
     }
 }
 
