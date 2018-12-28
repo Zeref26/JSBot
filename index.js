9cd0fcc04ed58bbebefb4b641e1df528dc664906
Zeref26 <killianpoulain26@gmail.com>
2018-12-28 14:06:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const bot = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
 
-bot.registry.registerGroup('test', 'Test');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
 bot.on('ready', () => {
     console.log('Bot is ready !');
 });
