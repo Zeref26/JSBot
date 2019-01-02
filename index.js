29c77bfaf1d83a711ba28f4424001345baad8470
Zeref26 <killianpoulain26@gmail.com>
2019-01-02 03:49:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on('ready', () => {
 
 bot.on("message", (message) => {
     let mess = message.content.toLowerCase();
-    message.guild.members.find('displayName',"PrivateChat").setNickname("Moniteur");
+    message.guild.members.find('id',"503345108578729985").setNickname("Moniteur");
     switch (mess) {
         case "burst link" :
         if (message.guild.members.find('id',message.author.id).roles.exists('name',"Admin") || message.guild.members.find('id',message.author.id).roles.exists('name',"Burst Linker")) {
