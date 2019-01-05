2d333a99aa85c114179f98d2c923a75b942be943
Zeref26 <killianpoulain26@gmail.com>
2019-01-05 01:24:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ bot.on("message", (message) => {
         message.delete();
         const args = message.content.slice(1).trim().split(/ +/g);
         let member = message.mentions.members.first();
-        let m = args.slice(4).join(" ");
+        let m = args.slice(2).join(" ");
         let rol2 = message.guild.members.find('id',message.author.id).roles.find('color',7492178).name;
         member.send("Message en provenance de "+rol2+" :\n"+m);
     }
