d2378d52cf74617136eabb246584e9c2a3a7faae
Zeref26 <killianpoulain26@gmail.com>
2019-01-11 15:25:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -400,8 +400,10 @@ bot.on("message", (message) => {
                 member.setNickname(args[2]+" "+args[3]);
             } else if (args.length==2) {
                 message.channel.send("Vous n'avez pas mis le nom.");
-            } else {
+            } else if (args.length==1) {
                 message.channel.send("Vous n'avez pas mis le prénom et le nom.");
+            } else {
+                message.channel.send("Il faut mentionnée une personne et entrer Prénom Nom.\nExemple : -nick "+message.guild.members.find('id',message.author.id)+" Jean-Luc Patrikac"); 
             }
         }
     }
@@ -410,7 +412,7 @@ bot.on("message", (message) => {
             message.delete();
             const args = message.content.slice(1).trim().split(/ +/g);
             if (args.length>=2) {
-                message.channel.bulkDelete(args[1]+1);
+                message.channel.bulkDelete(parseInt(args[1])+1);
             } else if (args.length<2) {
                 message.author.send("Vous n'avez pas entré le nombre de message à effacer.");
             }
