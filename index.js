796d7cce52fc8182579d53832272be172bade275
Zeref26 <killianpoulain26@gmail.com>
2019-01-23 14:45:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -464,7 +464,7 @@ bot.on("message", (message) => {
                     }
                 }
             }
-            message.channel.send("Voici les informations du serveur : \n\nNombre de salons : "+message.guild.channels.size+"\nNombre de rôles : "+message.guild.roles.size+"\n\n- Nombre de Burst Linker : "+bl+"\n - Nombre d'avatars rouges : "+rouge+"\n - Nombre d'avatars bleus : "+bleu+"\n - Nombre d'avatars jaunes : "+jaune+"\n - Nombre d'avatars verts : "+vert+"\n - Nombre d'avatars violets : "+violet+"\n - Nombre d'avatars noirs : "+noir+"\n - Nombre d'avatars blancs : "+blanc+"\n - Nombre d'avatars métalliques : "+metal+"\n - Nombre d'avatars transparents : "+transparent+"\n - Nombre d'avatars bicolores : "+bic+"\n\n - En attente de validation : "+valide+"\n En création d'avatar : "+enc+"\n En attente d'une fiche : "+(sf-3));
+            message.channel.send("Voici les informations du serveur : \n\nNombre de salons : "+message.guild.channels.size+"\nNombre de rôles : "+message.guild.roles.size+"\n\n- Nombre de Burst Linker : "+bl+"\n - Nombre d'avatars rouges : "+rouge+"\n - Nombre d'avatars bleus : "+bleu+"\n - Nombre d'avatars jaunes : "+jaune+"\n - Nombre d'avatars verts : "+vert+"\n - Nombre d'avatars violets : "+violet+"\n - Nombre d'avatars noirs : "+noir+"\n - Nombre d'avatars blancs : "+blanc+"\n - Nombre d'avatars métalliques : "+metal+"\n - Nombre d'avatars transparents : "+transparent+"\n - Nombre d'avatars bicolores : "+bic+"\n\n En attente de validation : "+valide+"\n En création d'avatar : "+enc+"\n En attente d'une fiche : "+(sf-3));
         }
     }
     if(message.content.startsWith("-nick")){
