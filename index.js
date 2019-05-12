08ccddfa06f404c79935280512abed6eae0fe024
Zeref26 <killianpoulain26@gmail.com>
2019-05-12 15:23:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ bot.on('ready', () => {
     console.log('Bot is ready !');
 });
 
-bot.on('guildMemberAvailable', member => {
-    bot.channels.find('id', "577115977083256833").send(member+" s'est connecté.");
+bot.on('presenceUpdate', (o_member,n_member) => {
+    if (o_member.presence.status == 'idle') { 
+        if (n_member.presence.status == 'online' || n_member.presence.status == 'dnd') {
+            bot.channels.find('id', "577115977083256833").send(member+" n'est plus AFK.");
+        }
+        if (n_member.presence.status == 'offline') {
+            bot.channels.find('id', "577115977083256833").send(member+" s'est déconnecté.");
+        }
+    } else if (n_member.presence.status == 'offline') {
+        bot.channels.find('id', "577115977083256833").send(member+" s'est déconnecté.");
+    } else if (o_member.presence.status == 'offline') {
+        bot.channels.find('id', "577115977083256833").send(member+" s'est connecté.");
+    }
 });
+
+bot.on('')
 
 bot.login(process.env.TOKEN);
