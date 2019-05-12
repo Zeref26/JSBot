4b6484eb4485312ebd6a0eb65185716944c38d97
Zeref26 <killianpoulain26@gmail.com>
2019-05-12 22:57:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -14,19 +14,20 @@ bot.on('ready', () => {
 });
 
 bot.on('presenceUpdate', (o_member,n_member) => {
-    if (o_member.presence.status == 'idle') { 
-        if (n_member.presence.status == 'online' || n_member.presence.status == 'dnd') {
-            bot.channels.find('id', "577115977083256833").send(n_member.displayName+" n'est plus AFK.");
-        }
-        if (n_member.presence.status == 'offline') {
+    if (!(o_member.id == '295196682268835851' || o_member.id == '373444058598735882')) {
+        if (o_member.presence.status == 'idle') { 
+            if (n_member.presence.status == 'online' || n_member.presence.status == 'dnd') {
+                bot.channels.find('id', "577115977083256833").send(n_member.displayName+" n'est plus AFK.");
+            } else if (n_member.presence.status == 'offline') {
+                bot.channels.find('id', "577115977083256833").send(n_member.displayName+" s'est déconnecté.");
+            }
+        } else if (n_member.presence.status == 'offline') {
             bot.channels.find('id', "577115977083256833").send(n_member.displayName+" s'est déconnecté.");
+        } else if (o_member.presence.status == 'offline') {
+            bot.channels.find('id', "577115977083256833").send(n_member.displayName+" s'est connecté.");
+        } else if (o_member.presence.status == 'online' && n_member.presence.status == 'idle') {
+            bot.channels.find('id', "577115977083256833").send(n_member.displayName+" est AFK.");
         }
-    } else if (n_member.presence.status == 'offline') {
-        bot.channels.find('id', "577115977083256833").send(n_member.displayName+" s'est déconnecté.");
-    } else if (o_member.presence.status == 'offline') {
-        bot.channels.find('id', "577115977083256833").send(n_member.displayName+" s'est connecté.");
-    } else if (o_member.presence.status == 'online' && n_member.presence.status == 'idle') {
-        bot.channels.find('id', "577115977083256833").send(n_member.displayName+" est AFK.");
     }
 });
 
